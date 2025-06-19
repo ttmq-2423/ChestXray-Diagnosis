bf110bf2026edb5ecd5373eb85e7249f931cc66c
ttmq-2423 <ttminhquynh@gmail.com>
2025-06-19 23:54:51 +0700

    fix

--- diagnosis-app/next.config.mjs
+++ diagnosis-app/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   images: { unoptimized: true },
 
   basePath: process.env.NODE_ENV === "production" ? "/ChestXray-Diagnosis" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/ChestXray-Diagnosis" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/ChestXray-Diagnosis/" : "",
 };
 
 export default nextConfig;
